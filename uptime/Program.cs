bf2f9700fe95a6c1ecfa5a63e687dc8f36e4425b
Gerald Büchler <dev@genomast.de>
2021-04-24 12:23:14 +0200

    Implement getVersionTxt - Method

--- uptime/Program.cs
+++ uptime/Program.cs
@@ -95,7 +95,17 @@ namespace uptime
 
         private static string getVersionTxt()
         {
-            throw new NotImplementedException();
+            ClsAbout about = new ClsAbout();
+
+            String msg = String.Empty;
+            msg += about.getProductName() + " - v" + about.getVersion();
+            msg += Environment.NewLine;
+            msg += "Author(s): " + about.getCompany();
+            msg += Environment.NewLine;
+            msg += "License: " + about.getCopyright();
+            msg += Environment.NewLine;
+
+            return msg;
         }
     }
 }
